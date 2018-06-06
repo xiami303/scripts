5f1b113d025a451dddb9ce0dde25f55d834b78ce
xiami303 <wuhongxia303@163.com>
2018-06-06 11:15:55 +0800

    Create check_parameters.bat
    check parameters passed in if all starts with "-", if not,  skip the build.

---
+++ bat/check_parameters.bat
@@ -0,0 +1,18 @@
+setlocal enabledelayedexpansion
+
+set build=0
+for %%a in (%*) do (
+	REM below check the parameters should start with -, if not goto eof
+	if not "!val:~0,1!" == "-" (
+		echo ERROR, you need to pass in parameters with format "-t" "-it"
+		goto :eof
+	) else ( 
+		set build=1
+	)
+)
+
+if %build%==1 (
+	echo will start the build from here ~~
+)
+
+pause
